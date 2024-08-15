4f6af771e3831031abd71a8da305b2b0409a77c7
Charlie Kim <15697110-KunjoongKim@users.noreply.replit.com>
2024-08-15 15:28:47 +0000

    added onClick to BasicButton

--- src/components/BasicButton/BasicButton.tsx
+++ src/components/BasicButton/BasicButton.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./BasicButton.scss";
 
-function BasicButton({ name }: { name: string }) {
-  return <button className="basicButtonWrapper">{name}</button>;
+export type ButtonTypes = {
+  name: string;
+  onClick?: any;
+};
+
+interface ButtonProps extends ButtonTypes {
+  onClick?: () => void;
+}
+
+function BasicButton({ name, onClick }: ButtonProps) {
+  return (
+    <button className="basicButtonWrapper" onClick={onClick}>
+      {name}
+    </button>
+  );
 }
 
 export default BasicButton;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,11 +3,12 @@ import "./App.scss";
 import BasicButton from "../components/BasicButton/BasicButton";
 
 function App() {
+  let buttonClick = () => console.log("button clicked");
   return (
     <div className="App">
       <header className="App-header">Hello Tanyak Laundry 3 with scss</header>
-      <div style={{ width: 300 }}>
-        <BasicButton name="로그인" />
+      <div style={{ width: 322, height: 48 }}>
+        <BasicButton name="로그인" onClick={buttonClick} />
       </div>
     </div>
   );
