71a7130516656898f75c5c99e152c425a8c7cfd1
Charlie Kim <15697110-KunjoongKim@users.noreply.replit.com>
2024-08-15 14:08:16 +0000

    created COMP-001 branch

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -4,7 +4,10 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello Tanyak Laundry 3</header>
+      <header className="App-header">Hello Tanyak Laundry 3 with scss</header>
+      <div style={{ width: 300 }}>
+        <button style={{ width: "100%" }}>Test</button>
+      </div>
     </div>
   );
 }
