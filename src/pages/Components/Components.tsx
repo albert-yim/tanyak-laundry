3e6151c19c5df77a4d6e174b1215676bfbc2d531
albert.yim <albert.yim719@gmail.com>
2024-08-15 23:19:25 +0900

    Add dummy pages for components and Login

--- src/pages/Components/Components.tsx
+++ src/pages/Components/Components.tsx
@@ -1,9 +1,5 @@
-import React from react;
+import React from "react";
 
 export default function Compoennts() {
-  return (
-  <div>
-      Compoentns
-    </div>
-  )
+  return <div>Compoentns</div>;
 }
