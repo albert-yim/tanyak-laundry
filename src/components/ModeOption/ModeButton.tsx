1c5395b948c80c1a649ccd25b8c84b32802e9566
albert.yim <albert.yim719@gmail.com>
2024-08-19 16:42:10 +0900

    Implement the logic to show options on ModeButton

--- src/components/ModeOption/ModeButton.tsx
+++ src/components/ModeOption/ModeButton.tsx
@@ -1,58 +1,58 @@
-import React, { MouseEventHandler, useState } from "react";
+import React, { MouseEventHandler, useEffect, useState } from "react";
 import styles from "./ModeButton.module.scss";
 import cn from "classnames";
 import ModeOption from "./ModeOption";
 
 type ModeButtonType = {
+  // Options to swho on ModeButton
+  modeOptions: ModeButtonOptionType;
+  // callback function when modebutton clicked
   onClick: MouseEventHandler;
 };
 
-export default function ModeButton({ onClick }: ModeButtonType) {
+type ModeButtonOptionType = {
+  left?: OptionType[];
+  right?: OptionType[];
+};
+type OptionType = {
+  // name of the mode
+  mode: string;
+  position: "top" | "center" | "bottom";
+  // whether the option is selected or not (default = false)
+  selected?: boolean;
+};
+
+export default function ModeButton({ modeOptions, onClick }: ModeButtonType) {
   const [mode, setMode] = useState<string>("강력세탁");
+  useEffect(() => {
+    Object.keys(modeOptions).map((key) => {
+      const lr = key as keyof ModeButtonOptionType;
+      const selectedOption = modeOptions[lr]?.find(
+        (option) => option?.selected,
+      );
+      if (selectedOption) setMode(selectedOption.mode);
+    });
+  }, [modeOptions]);
+
   return (
     <div className={styles.modeButtonWrapper}>
       <div className={styles.outerCircle}>
         <div className={styles.modeOptions}>
-          <div className={styles.left}>
-            <ModeOption
-              mode={"표준세탁"}
-              currentMode={mode}
-              setMode={setMode}
-              position={"left-top"}
-            />
-            <ModeOption
-              mode={"표준세탁"}
-              currentMode={mode}
-              setMode={setMode}
-              position={"left-center"}
-            />
-            <ModeOption
-              mode={"표준세탁"}
-              currentMode={mode}
-              setMode={setMode}
-              position={"left-bottom"}
-            />
-          </div>
-          <div className={styles.right}>
-            <ModeOption
-              mode={"표준세탁"}
-              currentMode={mode}
-              setMode={setMode}
-              position={"right-top"}
-            />
-            <ModeOption
-              mode={"표준세탁"}
-              currentMode={mode}
-              setMode={setMode}
-              position={"right-center"}
-            />
-            <ModeOption
-              mode={"표준세탁"}
-              currentMode={mode}
-              setMode={setMode}
-              position={"right-bottom"}
-            />
-          </div>
+          {Object.keys(modeOptions).map((key) => {
+            const lr = key as keyof ModeButtonOptionType;
+            return (
+              <div className={styles[lr]}>
+                {modeOptions[lr]?.map((option) => (
+                  <ModeOption
+                    mode={option.mode}
+                    currentMode={mode}
+                    setMode={setMode}
+                    position={`${lr}-${option.position}`}
+                  />
+                ))}
+              </div>
+            );
+          })}
         </div>
       </div>
       <div className={styles.innerCircle}></div>
@@ -62,6 +62,37 @@ export default function ModeButton({ onClick }: ModeButtonType) {
 
 export function ModeButtonTest() {
   const [option, setMode] = useState<string>("표준세탁");
+  const LAUNDRY_OPTIONS: ModeButtonOptionType = {
+    left: [
+      {
+        mode: "1",
+        position: "top",
+      },
+      {
+        mode: "2",
+        position: "center",
+      },
+      {
+        mode: "3",
+        position: "bottom",
+      },
+    ],
+    right: [
+      {
+        mode: "4",
+        position: "top",
+      },
+      {
+        mode: "5",
+        position: "center",
+      },
+      {
+        mode: "6",
+        position: "bottom",
+        selected: true,
+      },
+    ],
+  };
   return (
     <div
       style={{
@@ -73,7 +104,10 @@ export function ModeButtonTest() {
         height: 340,
       }}
     >
-      <ModeButton onClick={() => console.log("cc")} />
+      <ModeButton
+        onClick={() => console.log("cc")}
+        modeOptions={LAUNDRY_OPTIONS}
+      />
     </div>
   );
 }
