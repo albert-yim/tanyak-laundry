159a1e3f2d29256be28d0c82e061074dfcac2b8f
albert.yim <albert.yim719@gmail.com>
2024-09-03 23:39:53 +0900

    Add LaundryerButton component

--- src/components/LaunDryerButton/LaunDryerButton.tsx
+++ src/components/LaunDryerButton/LaunDryerButton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 import styles from "./LaunDryerButton.module.scss";
 import DRYER_ICON from "../../assets/dryer.svg";
@@ -7,8 +7,9 @@ import LAUNDRY_ICON from "../../assets/washingMachine.svg";
 type User = {
   id: string;
   name: string;
+  rank: "병장" | "상병" | "일병" | "이병";
 };
-type LaundDryerButton = {
+type LaunDryerButton = {
   type: "laundry" | "dryer";
   typeNumber: 1 | 2 | 3 | 4;
   user: User;
@@ -16,16 +17,72 @@ type LaundDryerButton = {
   endTime: string;
 };
 
-export default function LaundDryerButton() {
+export default function LaunDryerButton({
+  type,
+  typeNumber,
+  user,
+  startTime,
+  endTime,
+}: LaunDryerButton) {
+  const [typeName, typeIcon] = useMemo(() => {
+    let icon = LAUNDRY_ICON;
+    let name = "세탁기";
+    if (type === "dryer") {
+      icon = DRYER_ICON;
+      name = "건조기";
+    }
+    return [name, icon];
+  }, [type]);
+  const isUsed = useMemo(() => {
+    //TODO: Implement the logic to calculate the rest time
+    const isUsed = false;
+    return isUsed;
+  }, [endTime]);
   return (
     <div className={styles.wrapper}>
       <div className={styles.header}>
-        <img src={DRYER_ICON} alt="dryer icon"></img>
-        <span>1 dryer</span>
+        <div className={styles.iconWrapper}>
+          <img src={typeIcon} alt="dryer icon"></img>
+        </div>
+        <span>{`${typeNumber}번 ${typeName}`}</span>
       </div>
-      <span>Used</span>
-      <span>35m 25s</span>
-      <span>Kim MoSi</span>
+      <span className={styles.usage}>{isUsed ? "사용중" : "사용가능"}</span>
+      <span className={styles.time}>
+        {isUsed ? "35m 25s" : "마지막 사용: "}
+      </span>
+      <span>{`${user.rank} ${user.name}`}</span>
+    </div>
+  );
+}
+
+const USER1: User = {
+  id: "23-70006795",
+  name: "임찬양",
+  rank: "병장",
+};
+export function LaunDryerButtonTest() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        gap: 30,
+      }}
+    >
+      <LaunDryerButton
+        type="laundry"
+        typeNumber={1}
+        user={USER1}
+        startTime={"2024-09-03T23:23:23"}
+        endTime={"2024-09-03T23:59:59"}
+      />
+      <LaunDryerButton
+        type="dryer"
+        typeNumber={3}
+        user={USER1}
+        startTime={"2024-09-03T23:23:23"}
+        endTime={"2024-09-03T23:59:59"}
+      />
     </div>
   );
 }
