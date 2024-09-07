74bced342b0d6d5f2f0472cbb0e385394e8b80ea
Charlie Kim <90428123+Charlie0921@users.noreply.github.com>
2024-09-07 15:55:06 +0000

    added all necessary components on the page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,26 @@
-import React from "react";
-
-import BasicButton from "../../components/BasicButton/BasicButton";
+import React, { useState } from "react";
+import styles from "./Login.module.scss";
+import Button from "../../components/Button/Button";
+import Input from "../../components/Input/Input";
+import { ReactComponent as LOGOICON } from "../../assets/logo.svg";
 
 export default function Login() {
-  return <div>Login page</div>;
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  return (
+    <div className={styles.loginWrapper}>
+      <LOGOICON />
+      <span className={styles.text}>더욱 간단해진 /n 탄약중대 세탁 시스템</span>
+      <div className={styles.inputWrapper}>
+        <Input placeholder="이름" value={name} setValue={setName} />
+        <Input placeholder="군번" value={number} setValue={setNumber} />
+      </div>
+      <div className={styles.buttonWrapper}>
+        <Button
+          children="로그인"
+          onClick={() => console.log("button clicked")}
+        />
+      </div>
+    </div>
+  );
 }
