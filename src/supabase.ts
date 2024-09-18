456430c6b2fa3f26f9c898d4f767e5105a62eb49
albert.yim <albert.yim719@gmail.com>
2024-09-18 19:48:54 +0900

    Add supabase config file

---
+++ src/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://fbyylzqmgyinjqjoalng.supabase.co";
+
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY ?? "";
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
