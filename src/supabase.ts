9987111a1f3f09c3053a27eac6e0ce48760cf97f
albert.yim <albert.yim719@gmail.com>
2024-09-22 00:19:32 +0900

    update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,5 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-
 const supabaseUrl = "https://fbyylzqmgyinjqjoalng.supabase.co";
 
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY ?? "";
