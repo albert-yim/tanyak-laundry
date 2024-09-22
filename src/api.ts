80cc2be78c801bdd4bb4a67d0a16f8d6ed99f7c4
albert.yim <albert.yim719@gmail.com>
2024-09-22 19:28:22 +0900

    Add api calls for Insert history, fetch appliance and fetch modes

--- src/api.ts
+++ src/api.ts
@@ -1,58 +1,75 @@
+import {
+  convertApplianceModePayload,
+  convertAppliancesPayload,
+} from "./helper/payloadConvert";
 import { supabase } from "./supabase";
-import { UsageHistory, ApplianceMode } from "./types";
+import {
+  ApplianceMode,
+  Appliance,
+  AppliancePayload,
+  ApplianceModePayload,
+  UsageHistoryInsertPayload,
+} from "./types";
 
-export const fetchModes = async (): Promise<ApplianceMode[]> => {
-  const { data, error } = await supabase.from("ApplianceMode").select("*");
-  if (error) {
-    console.error("Error fetching modes:", error);
-    throw new Error(error.message);
-  }
-
-  return data;
-};
-
-export const fetchWasherModes = async (): Promise<ApplianceMode[]> => {
+export const fetchAppliances = async (): Promise<Appliance[]> => {
   const { data, error } = await supabase
-    .from("ApplianceMode")
-    .select("*")
-    .eq("appliance_type", "washing_machine");
+    .from("appliance")
+    .select(
+      `id,
+      location,
+      status,
+      type,
+      usage_history!last_usage_id(created_at, end_at, user(name))`,
+    )
+    .returns<AppliancePayload[]>();
   if (error) {
     console.error("Error fetching modes:", error);
-    throw new Error(error.message);
+    return [];
   }
 
-  return data;
+  return convertAppliancesPayload(data);
 };
+
+/*
+ * fetch washing_machine modes
+ */
+export const fetchWashingMachineModes = async (): Promise<ApplianceMode[]> => {
+  const { data, error } = await supabase
+    .from("appliance_mode")
+    .select("*")
+    .eq("appliance_type", "washing_machine")
+    .returns<ApplianceModePayload[]>();
+  if (error) {
+    console.error("Error fetching modes:", error);
+    return [];
+  }
+
+  return convertApplianceModePayload(data);
+};
+
+/*
+ * fetch dryer modes
+ */
 export const fetchDryerModes = async (): Promise<ApplianceMode[]> => {
   const { data, error } = await supabase
-    .from("ApplianceMode")
+    .from("appliance_mode")
     .select("*")
-    .eq("appliance_type", "dryer");
+    .eq("appliance_type", "dryer")
+    .returns<ApplianceModePayload[]>();
   if (error) {
     console.error("Error fetching modes:", error);
-    throw new Error(error.message);
+    return [];
   }
 
-  return data;
+  return convertApplianceModePayload(data);
 };
 
-const fetchWashingMachineHistory = async (): Promise<UsageHistory[]> => {
-  const { data, error } = await supabase.from("UsageHistory").select(`
-created_at,
-end_at,
-Appliance(type),
-User(id, name)
-`);
-  if (error) {
-    console.log("Error fetching history");
-  }
-  return data as any;
-};
-
-const insertUsageHistory = async (data: Omit<UsageHistory, "created_at">) => {
-  const { error } = await supabase.from("UsageHistory").insert([data]);
+/*
+ * Insert UsageHistory to Backend
+ */
+export const insertUsageHistory = async (data: UsageHistoryInsertPayload) => {
+  const { error } = await supabase.from("usage_history").insert([data]);
   if (error) {
     console.log("Error insert data in UsageHistory");
   }
 };
-export { fetchWashingMachineHistory, insertUsageHistory };
