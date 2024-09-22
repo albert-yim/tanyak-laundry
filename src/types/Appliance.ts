7da50a0839ebc6bce1a90b1e485245a7fdda042e
albert.yim <albert.yim719@gmail.com>
2024-09-22 23:32:47 +0900

    Remove unnecessary types

--- src/types/Appliance.ts
+++ src/types/Appliance.ts
@@ -33,25 +33,3 @@ export type UsageHistory = {
   // which mode is applied
   // mode_id: number;
 };
-
-export type Dryer = {
-  user: User;
-  mode: "regular" | "else";
-  startTime: string;
-  endTime: string;
-  position: 1 | 2 | 3 | 4;
-};
-
-export type Laundry = {
-  user: User;
-  mode:
-    | "표준세탁"
-    | "강력세탁"
-    | "청정세탁"
-    | "울/란제리"
-    | "합성섬유"
-    | "헹굼+탈수";
-  startTime: string;
-  endTime: string;
-  position: 1 | 2 | 3 | 4;
-};
