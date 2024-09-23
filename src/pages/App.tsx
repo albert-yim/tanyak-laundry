8c13c5e599f1411bd0d7b72d3d25c16d8b620558
albert.yim <albert.yim719@gmail.com>
2024-09-24 00:00:41 +0900

    implement the feature to signIn and signUp

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.scss";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import Compoennts from "./Components/Components";
 import Login from "./Login/Login";
 import Main from "./Main/Main";
 import { User } from "../types";
+import { signInWithId } from "../api";
+import { supabase } from "../supabase";
+import { Session } from "@supabase/supabase-js";
 
 const DemoUser: User = {
   id: "23-70001234",
@@ -14,6 +17,27 @@ const DemoUser: User = {
 };
 
 function App() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  // const navigate = useNavigate();
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+    supabase.auth.onAuthStateChange((_e, session) => {
+      setSession(session);
+    });
+  }, []);
+  useEffect(() => {
+    if (!session) {
+      // navigate("/login", { replace: true });
+
+      return;
+    }
+    // navigate("/", { replace: true });
+  }, [session]);
+  console.log("session");
+  console.log(session);
   return (
     <BrowserRouter>
       <Routes>
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,12 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Login.module.scss";
 import Button from "../../components/Button/Button";
 import Input from "../../components/Input/Input";
 import { ReactComponent as LOGOICON } from "../../assets/logo.svg";
+import { signInWithId, signUpWithUserData } from "../../api";
 
 export default function Login() {
   const [name, setName] = useState("");
   const [number, setNumber] = useState("");
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const loginButtonClicked = async () => {
+    if (number === "") {
+      //should handle error
+      console.log("empty number");
+      return;
+    }
+
+    if (isSignUp) {
+      //should show someting to indicate signUp
+      signUpWithUserData({
+        id: number,
+        name,
+        rank: "병장",
+        serviceStartedAt: "20230320T0000",
+      });
+      return;
+    }
+
+    const { error } = await signInWithId(number);
+    if (error) {
+      setIsSignUp(true);
+    }
+  };
+
   return (
     <div className={styles.loginWrapper}>
       <LOGOICON />
@@ -16,14 +43,15 @@ export default function Login() {
         탄약중대 세탁 시스템
       </span>
       <div className={styles.inputWrapper}>
-        <Input placeholder="이름" value={name} setValue={setName} />
         <Input placeholder="군번" value={number} setValue={setNumber} />
+        {isSignUp ? (
+          <Input placeholder="이름" value={name} setValue={setName} />
+        ) : (
+          <></>
+        )}
       </div>
       <div className={styles.buttonWrapper}>
-        <Button
-          children="로그인"
-          onClick={() => (location.href = "/Components")}
-        />
+        <Button onClick={loginButtonClicked}>로그인</Button>
       </div>
     </div>
   );
