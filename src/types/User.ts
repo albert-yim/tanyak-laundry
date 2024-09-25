de41b41bc814f560ef9eb92f0a47af0952cd3aaa
albert.yim <albert.yim719@gmail.com>
2024-09-25 23:05:05 +0900

    Update User type as backend

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,6 +1,13 @@
 export type User = {
-  id: string;
+  // auth id
+  // id: string;
+  // user name
   name: string;
-  rank: "병장" | "상병" | "일병" | "이병";
-  serviceStartedAt: string;
+  //기수
+  class: string;
+  //군번
+  serviceId: string;
+  // 입대날짜
+  serviceStartedAt?: string;
+  rank?: "병장" | "상병" | "일병" | "이병";
 };
