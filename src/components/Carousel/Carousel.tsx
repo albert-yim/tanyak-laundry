407359abafe2587ea830e2e4d0add2a2cd651768
albert.yim <albert.yim719@gmail.com>
2024-09-29 00:56:18 +0900

    Update position type name to location

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -4,53 +4,51 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export type CarouselTypes = {
-    contents: JSX.Element[];
+  contents: JSX.Element[];
 };
 
-export default function Carousel({contents}:CarouselTypes) {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow:1,
-        slidesToScroll:1,
-        arrows: false,
-
-    }
-    return(
-        <Slider {...settings}>
-        {contents.map(element => element
-            )}
-        </Slider>
-    );
+export default function Carousel({ contents }: CarouselTypes) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  };
+  return <Slider {...settings}>{contents.map((element) => element)}</Slider>;
 }
 
 export function CarouselTest() {
+  const FirstPage = (
+    <div
+      style={{
+        height: 300,
+        width: 400,
+        backgroundColor: "blue",
+      }}
+    >
+      First Page
+    </div>
+  );
 
-    const FirstPage = 
-        <div style = {{
-            height: 300,
-            width: 400,
-            backgroundColor: "blue"
-        }}>
-            First Page 
-        </div>
-    
-    const SecondPage =
-        <div style = {{
-            height: 300,
-            width: 400,
-            backgroundColor: "red"
-        }}>
-            Second Page 
-        </div>
+  const SecondPage = (
+    <div
+      style={{
+        height: 300,
+        width: 400,
+        backgroundColor: "red",
+      }}
+    >
+      Second Page
+    </div>
+  );
 
-    const PAGES = [FirstPage,SecondPage]
+  const PAGES = [FirstPage, SecondPage];
 
-    
-    return(
-        <>
-        <Carousel contents={PAGES} />
-        </>
-    );
+  return (
+    <>
+      <Carousel contents={PAGES} />
+    </>
+  );
 }
