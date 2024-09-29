490de70ff4823e42c9b68fe9f0ab18a67c85f124
albert.yim <albert.yim719@gmail.com>
2024-09-29 23:34:16 +0900

    Add user props on ModeModal button and handle to click start button

--- src/components/ModeModal/ModeModal.tsx
+++ src/components/ModeModal/ModeModal.tsx
@@ -1,12 +1,16 @@
-import React, { useState, ReactNode } from "react";
+import React, { useState, ReactNode, useEffect, useMemo } from "react";
 import styles from "./ModeModal.module.scss";
 import Modal from "../Modal/Modal";
 import ModeButton from "../ModeButton/ModeButton";
-import { Appliance, OptionType, ModeButtonOptionType } from "../../types";
+import { Appliance, OptionType, ModeButtonOptionType, User } from "../../types";
+import moment from "moment";
+import { insertUsageHistory } from "../../api";
 
 export type ModeModalTypes = {
   // callback function to close modal
-  onClose: () => void;
+  onClose: (refetch: boolean) => void;
+  // current user
+  user: User;
   // selected Appliance
   appliance: Appliance | null;
   // wheather modal is visible or not
@@ -15,6 +19,7 @@ export type ModeModalTypes = {
 
 export default function ModeModal({
   visible,
+  user,
   appliance,
   onClose,
 }: ModeModalTypes) {
@@ -22,31 +27,54 @@ export default function ModeModal({
     LAUNDRY_OPTIONS?.left?.[0]!,
   );
 
+  // set modeOptions depend on appliance type
+  const modeOptions =
+    appliance?.type === "dryer" ? DRYER_OPTIONS : LAUNDRY_OPTIONS;
+
+  useEffect(() => {
+    if (!!visible) {
+      // set selectedMode when modal is visible
+      setSelectedMode(modeOptions.left?.[0]!);
+    }
+  }, [visible]);
+
+  //function to return estimate end time with selectedMode
+  const getEndTime = (format: string = "") => {
+    const startTime = new Date();
+    return moment(startTime)
+      .add(selectedMode.duration, "minutes")
+      .local()
+      .format(format);
+  };
+
+  // insert data on the backend and close modal
+  const modeButtonClicked = () => {
+    if (!appliance?.id) {
+      console.log("[ERROR] Insert usageHistory: aid cannot be empty stirng");
+      return;
+    }
+
+    insertUsageHistory({
+      uid: user.id,
+      aid: appliance.id,
+      end_time: getEndTime(),
+    });
+    onClose(true);
+  };
+
+  // return empty node if appliance is null
   if (appliance == null) return <></>;
 
-  const modeOptions =
-    appliance.type === "dryer" ? DRYER_OPTIONS : LAUNDRY_OPTIONS;
-  const typeName = appliance.type === "dryer" ? "건조기" : "세탁기";
-
-  const currentDate = new Date();
-  const calculatedTime = currentDate.setMinutes(
-    currentDate.getMinutes() + selectedMode.duration,
-  );
-  const calculatedDate = new Date(calculatedTime);
-  const calculatedDateHours =
-    (calculatedDate.getHours() < 10 ? "0" : "") + calculatedDate.getHours();
-  const calculatedDateMinutes =
-    (calculatedDate.getMinutes() < 10 ? "0" : "") + calculatedDate.getMinutes();
-
   return (
-    <Modal onClose={onClose} visible={visible}>
+    <Modal onClose={() => onClose(false)} visible={visible}>
       <div className={styles.contentWrapper}>
         <div className={styles.locationText}>
-          {appliance.location}번 {typeName}
+          {appliance.location}번{" "}
+          {appliance.type === "dryer" ? "건조기" : "세탁기"}
         </div>
         <div className={styles.modeButton}>
           <ModeButton
-            onClick={onClose}
+            onClick={modeButtonClicked}
             modeOptions={modeOptions}
             selectedMode={selectedMode}
             setSelectedMode={setSelectedMode}
@@ -54,9 +82,7 @@ export default function ModeModal({
           />
         </div>
         <div className={styles.durationText}>{selectedMode.duration}m</div>
-        <div className={styles.endTimeText}>
-          {calculatedDateHours}:{calculatedDateMinutes}
-        </div>
+        <div className={styles.endTimeText}>{getEndTime("HH:mm")}</div>
       </div>
     </Modal>
   );
@@ -66,35 +92,24 @@ const DRYER_OPTIONS: ModeButtonOptionType = {
   left: [
     {
       mode: "표준건조",
-
       position: "top",
-
       duration: 45,
     },
-
     {
       mode: "강력건조",
-
       position: "bottom",
-
       duration: 45,
     },
   ],
-
   right: [
     {
       mode: "섬세의류",
-
       position: "top",
-
       duration: 45,
     },
-
     {
       mode: "10분건조",
-
       position: "bottom",
-
       duration: 45,
     },
   ],
@@ -104,25 +119,17 @@ const LAUNDRY_OPTIONS: ModeButtonOptionType = {
   left: [
     {
       mode: "표준세탁",
-
       position: "top",
-
       duration: 36,
     },
-
     {
       mode: "강력세탁",
-
       position: "center",
-
       duration: 50,
     },
-
     {
       mode: "청정세탁",
-
       position: "bottom",
-
       duration: 36,
     },
   ],
@@ -130,25 +137,17 @@ const LAUNDRY_OPTIONS: ModeButtonOptionType = {
   right: [
     {
       mode: "물/란제리",
-
       position: "top",
-
       duration: 36,
     },
-
     {
       mode: "합성섬유",
-
       position: "center",
-
       duration: 36,
     },
-
     {
       mode: "헹굼+탈수",
-
       position: "bottom",
-
       duration: 36,
     },
   ],
