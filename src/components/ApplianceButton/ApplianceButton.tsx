b90f289738240c128fe629895a181b49bca04118
albert.yim <albert.yim719@gmail.com>
2024-10-01 16:34:57 +0900

    Implement the feature to show remaingTime depend on appliance endTime

--- src/components/ApplianceButton/ApplianceButton.tsx
+++ src/components/ApplianceButton/ApplianceButton.tsx
@@ -1,9 +1,9 @@
-import React, { useMemo } from "react";
-
+import React, { useEffect, useMemo, useState } from "react";
 import styles from "./ApplianceButton.module.scss";
 import { ReactComponent as DRYER_ICON } from "../../assets/dryer.svg";
 import { ReactComponent as LAUNDRY_ICON } from "../../assets/washingMachine.svg";
 import { UsageHistory } from "../../types";
+import moment from "moment";
 
 type ApplianceButtonType = {
   onClick: () => void;
@@ -18,19 +18,53 @@ export default function ApplianceButton({
   location,
   lastUsage,
 }: ApplianceButtonType) {
+  //remainingTime in sec
+  const [remainingTime, setRemainingTime] = useState<number>(0);
+  const isUsed = remainingTime > 0;
   const typeName = type === "dryer" ? "건조기" : "세탁기";
   const typeIcon =
     type === "dryer" ? <DRYER_ICON width="15" /> : <LAUNDRY_ICON width="15" />;
 
-  const { endTime, user } = lastUsage;
-  const isUsed = useMemo(() => {
-    //TODO: Implement the logic to calculate the rest time
-    const isUsed = false;
-    return isUsed;
-  }, [endTime]);
+  function calTimeDiffInSec(endTime: string) {
+    const timeDiff = moment(endTime).diff(moment(), "second");
+    if (endTime === "" || timeDiff <= 0) return 0;
+    console.log("caltimer");
+    console.log(moment().toLocaleString());
+    console.log(moment(endTime).toLocaleString());
+    console.log(timeDiff);
+    return timeDiff;
+  }
+
+  useEffect(() => {
+    console.log("useEffect lastUsage");
+    const timeDiff = calTimeDiffInSec(lastUsage.endTime);
+    if (timeDiff <= 0) return;
+    setRemainingTime(timeDiff);
+
+    //set timer to re calculate remainingTime every 10sec
+    const timer = setInterval(() => {
+      console.log("Interval");
+      const timeDiff = calTimeDiffInSec(lastUsage.endTime);
+      setRemainingTime(timeDiff);
+
+      if (timeDiff <= 0) {
+        setRemainingTime(0);
+        clearInterval(timer);
+      }
+    }, 10 * 1000);
+
+    // timer clear when unmount compount
+    return () => clearInterval(timer);
+  }, [lastUsage]);
+
   return (
     <div className={styles.wrapper} onClick={onClick}>
-      <div className={styles.progress} />
+      <div
+        className={styles.progress}
+        style={{
+          height: isUsed ? 0 : "100%",
+        }}
+      />
 
       <div className={styles.contentsWrapper}>
         <div className={styles.header}>
@@ -39,9 +73,13 @@ export default function ApplianceButton({
         </div>
         <span className={styles.usage}>{isUsed ? "사용 중" : "사용 가능"}</span>
         <span className={styles.time}>
-          {isUsed ? "35m 25s" : "마지막 사용: "}
+          {isUsed
+            ? `남은시간: ${Math.ceil(remainingTime / 60)}분`
+            : "마지막 사용: "}
         </span>
-        <span className={styles.name}>{`${user.class}기 ${user.name}`}</span>
+        <span
+          className={styles.name}
+        >{`${lastUsage.user.class}기 ${lastUsage.user.name}`}</span>
       </div>
     </div>
   );
