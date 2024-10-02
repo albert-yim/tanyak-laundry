36ed86dcea6e2b92e0119c386fdb01206e9d3832
albert.yim <albert.yim719@gmail.com>
2024-10-02 17:57:46 +0900

    Add config file for react-app-rewired package

---
+++ config-overrides.js
@@ -0,0 +1,5 @@
+const { aliasWebpack, aliasJest } = require("react-app-alias");
+
+const options = {}; // default is empty for most cases
+module.exports = aliasWebpack(options);
+module.exports.jest = aliasJest(options);
