bc0fb50ef3723cc4cc2641ec4d5529df41f4035e
Kunjoong(Charlie) Kim <90428123+Charlie0921@users.noreply.github.com>
2024-10-06 15:23:02 +0000

    edited TimePicker css

--- src/components/TimePicker/TimePicker.tsx
+++ src/components/TimePicker/TimePicker.tsx
@@ -1,45 +1,71 @@
-import React, {useState} from "react";
+import React, {useState,useRef, useEffect} from "react";
 import styles from "./TimePicker.module.scss";
 
 type TimePickerTypes = {
-    defaultValue: number;
-    values: { value: number; label:string }[];
+    
 }
 
-export default function TimePicker({ values, defaultValue} : TimePickerTypes)  {
-    const [scrollPosition, setScrollPosition] = useState(0);
+export default function TimePicker({} : TimePickerTypes)  {
+    const [selectedNumber, setSelectedNumber] = useState(0);
+    const numbers = Array.from({ length: 60 }, (_, i) => i);
+    const defaultNumber = 36;
+    const itemRefs = useRef<HTMLDivElement>(null);
+    const ITEM_HEIGHT = 30;
 
+    // const handleScroll = () => {
+    //     if (ref.current) {
+    //         if (ref.current.scrollTop < ITEM_HEIGHT) {
+    //             ref.current.scrollTop = ITEM_HEIGHT
+    //         }
+    //         const index = Math.floor(
+    //             (ref.current!.scrollTop | ITEM_HEIGHT / 2) /ITEM_HEIGHT);
+    //             if (list[index] !== "") {
+    //                 setSelectedNumber(index);
+    //                 itemRefs.current[index]?.scrollIntoView({
+    //                     behavior: "smooth",
+    //                     block: "center"
+    //                 })
+    //                 onSelectedChange && onSelectedChange(newList[index])
+    //             }
+    //         )
+    //     }
+    // }
+    
     return(
-        <div className={styles.timePickerWrapper}>
-            <div className={styles.numbersWrapper}>
-                {values.map((v,i) => {
+        <div className={styles.timePickerWrapper} >
+            <div className={styles.numbersWrapper} ref={itemRefs} onScroll={()=> {
+                const xpos = itemRefs.current?.scrollLeft ?? -1
+                if (xpos < 0) return
+                console.log(xpos/30)
+                setSelectedNumber(xpos/30)
+            }}>
+                {numbers.map((number) => {
                     return (
-                        <div key={v.value} className={styles.labelWrapper}>
-                            <span>{v.label}</span>
+                        <div 
+                            id={`${number}}`} 
+                            className={styles.labelWrapper}
+                            onClick={() => {
+                                itemRefs?.current?.scrollTo({
+                                    left: 30 * (number-2),
+                                    behavior: "smooth",
+                                })
+                                setSelectedNumber(number)}}
+                        >
+                            <span>{number}</span>
                         </div>
                     )
                 })}
             </div>
-            <div className="action-btns">
-                <button>Scroll Left</button>
-                <button>Scroll Right</button>
-            </div>
+            <div className={styles.vector}/>
+            <div className={styles.text}>{selectedNumber}</div>
         </div>
     )
 }
 
 export function TimePickerTest(){
-    const defaultValue = 36;
-    const start = 0
-    const values = new Array(60)
-    .fill(0)
-    .map((_,i) => {
-        const value = start + i;
-        return {value, label: `${value}`};
-    })
     return(
         <div>
-            <TimePicker values={values} defaultValue={defaultValue}/>
+            <TimePicker/>
         </div>
     )
 } 
