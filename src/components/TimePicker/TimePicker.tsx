b940b8c857f12aee5b42c0160f99c6a877c5447f
Kunjoong(Charlie) Kim <90428123+Charlie0921@users.noreply.github.com>
2024-10-08 16:21:32 +0000

    added conditional styling by utilizing classnames

--- src/components/TimePicker/TimePicker.tsx
+++ src/components/TimePicker/TimePicker.tsx
@@ -1,5 +1,6 @@
 import React, {useState,useRef, useEffect} from "react";
 import styles from "./TimePicker.module.scss";
+import cn from "classnames";
 
 type TimePickerTypes = {
     duration: number;
@@ -9,32 +10,47 @@ type TimePickerTypes = {
 
 export default function TimePicker({duration, setDuration} : TimePickerTypes)  {
     //list necessary numbers for wheel component
-    const numbers = [null,null,...Array.from({ length: 60 }, (_, i) => i),null,null];\
+    const numbers = [null,null,...Array.from({ length: 60 }, (_, i) => i),null,null];
 
     //use div element for referencing a value
     const itemRefs = useRef<HTMLDivElement>(null);
-
+    
+    let xposNumber = 0
     //scroll automatically towards default duration number while rendering
     useEffect(() => {
         itemRefs?.current?.scrollTo({
             left: 30 * duration,
         })
-    })
+        
+    },[])
+        
 
     return(
         <div className={styles.timePickerWrapper} >
-            <div className={styles.numbersWrapper} ref={itemRefs}>
+            <div className={styles.numbersWrapper} 
+                ref={itemRefs}
+                onScroll = {() => {
+                    const xpos = itemRefs?.current?.scrollLeft ?? -1
+                        if (xpos < 0) return
+                        xposNumber = xpos/30
+                        console.log(xposNumber)
+                        setDuration(xposNumber)
+                }}
+                >
                 {numbers.map((number) => {
                     return (
                         <div 
-                            id={`${number}}`} 
-                            className={styles.labelWrapper}
+                            id={`${number}`} 
+                            className={cn(styles.labelWrapper, {
+                                [styles.labelWrapper__selected]: (number === duration),
+                            })}
                             onClick={() => {
                                 itemRefs?.current?.scrollTo({
                                     left: 30 * number!,
                                     behavior: "smooth",
                                 })
-                                setDuration(number!)}}
+                                setDuration(number!)
+                            }}
                         >
                             <span>{number}</span>
                         </div>
