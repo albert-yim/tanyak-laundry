dae1670bc0d8e88aab46c06e22ed5a99717e08a2
Kunjoong(Charlie) Kim <90428123+Charlie0921@users.noreply.github.com>
2025-01-30 14:55:20 +0000

    made accordian foldable

--- src/components/Accordian/QuestionSection.tsx
+++ src/components/Accordian/QuestionSection.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./QuestionSection.module.scss";
+import { ReactComponent as DOWNARROW_ICON } from "@assets/downArrow.svg";
 
 export default function QuestionSection({ questionContent }: any) {
+  const [isAnswerVisible, setIsAnswerVisible] = useState(false);
   return (
     <div className={styles.questionSectionWrapper}>
-      <div className={styles.questionWrapper}>{questionContent.question}</div>
-      <div className={styles.answerWrapper}>{questionContent.answer}</div>
+      <div
+        className={styles.questionWrapper}
+        onClick={() => setIsAnswerVisible(!isAnswerVisible)}
+      >
+        {questionContent.question}
+        <DOWNARROW_ICON width="20px" height="auto" />
+      </div>
+      {isAnswerVisible ? (
+        <div className={styles.answerWrapper}>{questionContent.answer}</div>
+      ) : null}
     </div>
   );
 }
